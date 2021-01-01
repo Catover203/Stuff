38988d14b56fa42304f7acc9045ea78fe024ab64
Catover203 Creator <63941519+Catover203@users.noreply.github.com>
2021-01-01 13:00:08 +0700

    Create protect-img.js

---
+++ protect-img.js
@@ -0,0 +1,8 @@
+document.write(unescape('%3Cscript src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"%3E%3C/script%3E')
+function PLOGO(x){
+  $($x).mousedown(function (e) {
+    if(e.button == 2) {
+     alert('This Logo is protected');
+    }
+  }
+}
