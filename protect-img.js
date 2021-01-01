497f371d962b22a4db741e9ef2c42c1899560255
Catover203 Creator <63941519+Catover203@users.noreply.github.com>
2021-01-01 13:10:45 +0700

    Update protect-img.js

--- protect-img.js
+++ protect-img.js
@@ -1,6 +1,6 @@
-document.write(unescape('%3Cscript src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"%3E%3C/script%3E');
+document.write(unescape('%3Cscript src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"%3E%3C/script%3E'));
 function logo(url, link_to){
-  document.write(unescape('%3Ca href="' + link_to + '"%3E%3Cimg id="protected-logo" src="' + url + '"%3E%3E%3C/a%3E');
+  document.write(unescape('%3Ca href="' + link_to + '"%3E%3Cimg id="protected-logo" src="' + url + '"%3E%3E%3C/a%3E'));
   $('#protected-logo').mousedown(function (e) {
     if(e.button == 2) {
      alert('This Logo is protected');
